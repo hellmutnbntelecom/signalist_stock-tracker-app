e37c9b7cc4fa19bbcc61b36ec36c65df37d002b2
Hellmut Schuster <hellmut.nbntelecom@gmail.com>
2025-10-03 10:25:38 -0400

    implement header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 
 const Home = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex min-h-screen home-wrapper">
       <Button>
-        Click Me
+        Home
       </Button>
     </div>
   )
